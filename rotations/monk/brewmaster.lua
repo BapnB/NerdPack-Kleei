4ce509b80e29a88c9327625e932b8897dc3126e0
Kleei <vasile.xaker@gmail.com>
2017-07-22 13:25:06 +0300

    adaptation Brewmaster CR for lastest NeP commit

--- rotations/monk/brewmaster.lua
+++ rotations/monk/brewmaster.lua
@@ -21,55 +21,55 @@ end
 
 local Keybinds = {
 
-    {'%pause', 'keybind(alt)', 'player'},
-	{'Leg Sweep', 'keybind(shift) & target.range <=5 & target.enemy & target.alive'},
-	{'Paralysis', 'keybind(shift) & target.range >=10 & target.enemy & target.alive'},
-	{'Exploding Keg', 'keybind(control) & target.enemy & target.alive', 'cursor.ground'},
+    {"%pause", "keybind(alt)", "player"},
+	{"Leg Sweep", "keybind(shift) & talent(4,3) & target.range <= 5 & target.enemy & target.alive"},
+	{"Paralysis", "keybind(shift) & target.range >= 10 & target.enemy & target.alive"},
+	{"Exploding Keg", "keybind(control) & target.enemy & target.alive", "cursor.ground"},
 	
 }
 
 local Interrupts ={
 
-    {'Spear Hand Strike', 'target.range <=5'},
-	{'Paralysis', 'target.range >=6 || player.spell(Spear Hand Strike).cooldown >gcd'},
+    {"Spear Hand Strike", "target.range <= 5"},
+	{"Paralysis", "target.range >= 6 || player.spell(Spear Hand Strike).cooldown > gcd"},
 
 }
 
 local Survival = {
 
-	{'Fortifying Brew', 'player.health <= 35'},	
-	{'#Healthstone', 'player.health <=60'},
-	{'Healing Elixir', 'player.health <= 75 & !lastcast(Healing Elixir)'},
-    {'Black Ox Brew', '!player.buff(Ironskin Brew) & player.spell(Ironskin Brew).charges<1'},
-	{'Expel Harm', 'player.health <= 80'},
-	{'Chi Wave', 'player.health <= 90', 'player'},
-	{'Ironskin Brew', 'player.buff(Ironskin Brew).duration <=1 & player.spell(Ironskin Brew).charges >=2 & !lastcast(Ironskin Brew)'},-- Ironskin Brew 
+	{"Fortifying Brew", "player.health <= 35"},	
+	{"#Healthstone", "player.health <= 60"},
+	{"Healing Elixir", "player.health <= 75 & !lastcast(Healing Elixir)"},
+    {"Black Ox Brew", "!player.buff(Ironskin Brew) & player.spell(Ironskin Brew).charges < 1"},
+	{"Expel Harm", "player.health <= 80"},
+	{"Chi Wave", "talent(1,3) & player.health <= 90", "player"},
+	{"Ironskin Brew", "player.buff(Ironskin Brew).duration <= 1 & player.spell(Ironskin Brew).charges >= 2 & !lastcast(Ironskin Brew)"},
 
 }
 
 local Cooldowns = {
 
-   {'Rushing Jade Wind', '!player.buff(Rushing Jade Wind)'},
+   {"Rushing Jade Wind", "!player.buff(Rushing Jade Wind)"},
 
 }
 
 local Action = {
 
-	{'/startattack', '!isattacking & target.range <10'},
+	{"/startattack", "!isattacking & target.range < 10"},
 	
 	-- AoE
-	{'Breath of Fire', 'toggle(AoE) & equipped(Sal\'salabim\'s Lost Tunic) & target.range <=8'},         -- legendary chest (Sal'salabim's Lost Tunic) are equipped
-	{'Keg Smash', 'toggle(AoE) & player.area(8).enemies >=2'},
-	{'Rushing Jade Wind', 'toggle(AoE) & player.area(8).enemies >=2 & target.range <=8'},
-	{'Chi burst', 'toggle(AoE) & talent(1,1) & player.area(8).enemies >=2'},
-	{'Breath of Fire', 'toggle(AoE) & player.area(8).enemies >=2'},
+	{"Breath of Fire", "toggle(AoE) & equipped(Sal'salabim's Lost Tunic) & target.range <= 8"},         -- legendary chest (Sal'salabim's Lost Tunic) are equipped
+	{"Keg Smash", "toggle(AoE) & player.area(8).enemies >= 2"},
+	{"Rushing Jade Wind", "toggle(AoE) & player.area(8).enemies >= 2 & target.range <= 8"},
+	{"Chi burst", "toggle(AoE) & talent(1,1) & player.area(8).enemies >= 2"},
+	{"Breath of Fire", "toggle(AoE) & player.area(8).enemies >= 2"},
 	--Solo
-	{'Blackout Strike', 'target.range <=5'},
-	{'Breath of Fire', 'equipped(Sal\'salabim\'s Lost Tunic) & target.range <=8'},         -- legendary chest (Sal'salabim's Lost Tunic) are equipped
-	{'Keg Smash'},										
-	{'Tiger Palm', 'player.energy >= 65'},
-	{'Breath of Fire'},									          
-	{'Rushing Jade Wind'},								              
+	{"Blackout Strike", "target.range <= 5"},
+	{"Breath of Fire", "equipped(Sal'salabim's Lost Tunic) & target.range <= 8"},         -- legendary chest (Sal'salabim's Lost Tunic) are equipped
+	{"Keg Smash"},										
+	{"Tiger Palm", "player.energy >= 65"},
+	{"Breath of Fire"},									          
+	{"Rushing Jade Wind"},								              
 
 }
 
@@ -77,20 +77,20 @@ local inCombat = {
 
 	{Keybinds},
 	
-	{'%taunt(Provoke)'},
+	{"%taunt(Provoke)"},
 	
-	{Interrupts, 'target.interruptAt(15) & target.enemy & target.alive'},
-	{Survival, 'player.health < 100 & target.enemy & target.alive'},
-	{Cooldowns, 'toggle(cooldowns) & target.enemy & target.alive'},
-	{Action, 'target.range <=10 & target.enemy & target.alive'},
+	{Interrupts, "target.interruptAt(15) & target.enemy & target.alive"},
+	{Survival, "player.health < 100 & target.enemy & target.alive"},
+	{Cooldowns, "toggle(cooldowns) & target.enemy & target.alive"},
+	{Action, "target.range <= 10 & target.enemy & target.alive"},
 
 }
 
 local outCombat = {
 
     --{'%ressdead(Resuscitate)'},
-	{'%dispelall'},
-    {'Resuscitate', '!target.enemy & target.dead', 'target'},
+	{"%dispelall"},
+    {"Resuscitate", "!target.enemy & target.dead", "target"},
 	{Keybinds},
 	
 }
